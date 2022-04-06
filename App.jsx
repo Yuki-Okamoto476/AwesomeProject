a23972c4e135d20dfe6c03269710e03981adede2
OKAMOTO YUKI <okamotoyuki@OKAMOTOnoMacBook-Air.local>
2022-04-06 21:43:19 +0900

    Make component

--- App.jsx
+++ App.jsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
       <Text>your app!</Text>
       <StatusBar style="auto" />
     </View>
