0c9d0ccf27f71da7c089a2ff2c034d5c52cc66ae
OKAMOTO YUKI <okayu110108@gmail.com>
2022-04-13 08:20:21 +0900

    Implement logout function

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import CircleButton from '../components/CircleButton';
+import LogoutButton from '../components/LogOutButton';
 import MemoList from '../components/MemoList';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  useEffect(() => {
+    navigation.setOptions({
+      // eslint-disable-next-line react/no-unstable-nested-components
+      headerRight: () => <LogoutButton />,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <MemoList />
