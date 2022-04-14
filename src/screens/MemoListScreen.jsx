8a51ca7557973f18f8ff1b984e76dec459b271b6
OKAMOTO YUKI <okayu110108@gmail.com>
2022-04-15 07:23:43 +0900

    Fetch Data from firesrote and pass it to FlatList

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Alert } from 'react-native';
+
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import LogoutButton from '../components/LogOutButton';
@@ -7,6 +9,7 @@ import MemoList from '../components/MemoList';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
   useEffect(() => {
     navigation.setOptions({
       // eslint-disable-next-line react/no-unstable-nested-components
@@ -14,12 +17,42 @@ export default function MemoListScreen(props) {
     });
   }, []);
 
+  useEffect(() => {
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      const ref = db
+        .collection(`users/${currentUser.uid}/memos`)
+        .orderBy('updateAt', 'desc');
+      unsubscribe = ref.onSnapshot((snapshot) => {
+        const userMemos = [];
+        snapshot.forEach((doc) => {
+          console.log(doc.id, doc.data());
+          const data = doc.data();
+          userMemos.push({
+            id: doc.id,
+            bodyText: data.bodyText,
+            updateAt: data.updateAt.toDate(),
+          });
+        });
+        setMemos(userMemos);
+      }, (error) => {
+        console.log(error);
+        Alert.alert('データの読み込みに失敗しました。');
+      });
+    }
+    return () => unsubscribe();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MemoList />
+      <MemoList memos={memos} />
       <CircleButton
         name="plus"
-        onPress={() => { navigation.navigate('MemoCreate'); }}
+        onPress={() => {
+          navigation.navigate('MemoCreate');
+        }}
       />
     </View>
   );
